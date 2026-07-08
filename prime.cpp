4d32593bf1ebdef6f0cac61cf05144d1ee0f1682
Raavee Singh <gdg1465@gmail.com>
2026-07-08 12:22:04 +0530

    prime

---
+++ prime.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+int main(){
+    int n;
+    int i=2;
+    std::cout<<"Enter the value of n:";
+    std::cin>>n;
+    while(i<n){
+        if(n%2==0){
+            std::cout<<"not prime"<<std::endl;
+            break;
+        }
+        else{
+            std::cout<<"prime"<<std::endl;
+            break;
+        }
+        i++;
+
+    }
+}
