cddaa8106f43fd43a7ee1c2612d7d8652cf4c759
Raavee Singh <gdg1465@gmail.com>
2026-07-08 20:37:12 +0530

    additions

---
+++ patterns/pattern9.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+int main(){
+    int n;
+    std::cout<<"Enter the value of n: ";
+    std::cin>>n;
+    int i=1;
+
+    while(i<=n){
+        int j=1;
+        int count=i;
+        while(j<=i){
+            std::cout<<" "<<count<<" ";
+            count+=1;
+            j+=1;
+        }
+        std::cout<<std::endl;
+        i+=1;
+    }
+}
